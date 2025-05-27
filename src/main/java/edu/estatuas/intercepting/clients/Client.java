887df2d535d73822b5469c55b03bc23f02045149
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-05-27 18:40:14 +0200

    feat(Clients): :sparkles: setup Client interface

--- src/main/java/edu/estatuas/intercepting/clients/Client.java
+++ src/main/java/edu/estatuas/intercepting/clients/Client.java
@@ -1,4 +1,8 @@
 package edu.estatuas.intercepting.clients;
 
+import edu.estatuas.intercepting.admin.TaskProgrammer;
+
 public interface Client {
+    void setTaskProgrammer(TaskProgrammer taskProgrammer);
+    void sendPetition(String username);
 }
