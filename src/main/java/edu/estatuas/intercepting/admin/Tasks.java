1a9e0320c71a0aaab91e3c1d53d4544133b64b2b
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-05-27 18:10:08 +0200

    feat(Tasks):  :feat: add simple class attributes and task handling

--- src/main/java/edu/estatuas/intercepting/admin/Tasks.java
+++ src/main/java/edu/estatuas/intercepting/admin/Tasks.java
@@ -1,4 +1,29 @@
 package edu.estatuas.intercepting.admin;
 
+import edu.estatuas.intercepting.filters.Filter;
+import edu.estatuas.intercepting.targets.Target;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tasks {
+    private List<Filter> tasklist = new ArrayList<Filter>();
+
+    private Target target = null;
+
+    public List<Filter> getTasklist() { return tasklist; }
+
+    public Target getTarget() { return target; }
+
+    public void setTarget(Target target) { this.target = target; }
+
+    public void addTask(Filter task) { tasklist.add(task); }
+
+    public void execute(String username){
+
+    }
+
+
 }
+
+
