2814c33f89c38a9968d9077349c2c243ad6c71da
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-05-27 18:13:44 +0200

    feat(Target):  :feat: setup Vehicle class with some flavour

--- src/main/java/edu/estatuas/intercepting/targets/Vehicle.java
+++ src/main/java/edu/estatuas/intercepting/targets/Vehicle.java
@@ -1,4 +1,7 @@
 package edu.estatuas.intercepting.targets;
 
 public class Vehicle {
+    public void execute(String username){
+        System.out.println("Ye aquí su corcel, " + username + '.');
+    }
 }
